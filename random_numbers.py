bdd5bdaa5a554ecb6badc5b9e0138dd770f5e85b
TyPytheezy <tyryancraft@gmail.com>
2025-10-31 22:43:45 -0400

    git commit -m "Completed all lab programs and passed tests"

--- random_numbers.py
+++ random_numbers.py
@@ -1,4 +1,5 @@
 # TODO import the library to randomly generate numbers
+import random
 
 
 def generator(n, starting, ending):
@@ -16,6 +17,9 @@ def generator(n, starting, ending):
     numbers = []
 
     # TODO: use loop to generate n random integers between starting and ending, then append the random number into the list
+    for _ in range(n):
+        rand_num = random.randint(starting, ending)
+        numbers.append(rand_num)
 
     return numbers
 
